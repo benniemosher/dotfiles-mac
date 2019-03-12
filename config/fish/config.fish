17971848f969a14f5fae6249cce6e4aaae0c8062
Bennie Mosher <benniemosher@gmail.com>
2019-03-12 14:26:00 -0600

    Update color theme settings

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -15,16 +15,11 @@ else
     eval (gpg-agent --daemon | sed -e "s/\(.*\)=\(.*\); export/set -x \1 \2/")
 end
 
-<<<<<<< Updated upstream
-bass source ~/.nvm/nvm.sh --no-use ';' nvm use --delete-prefix $NODE_VERSION
-=======
-source ~/.config/fish/local.config.fish
->>>>>>> Stashed changes
-
+# Set default version of Node
 source ~/.config/fish/nvm-wrapper/nvm.fish
-# nvm use $NODE_VERSION
 nvm use --delete-prefix v$NODE_VERSION --silent
 
+# Set default version of Ruby
 source /usr/local/share/chruby/chruby.fish
 source /usr/local/share/chruby/auto.fish
 chruby $RUBY_VERSION
@@ -43,27 +38,27 @@ set -g theme_display_git_master_branch yes
 #
 # std prompt config
 #
-# set __fish_prompt_color_carat (set_color -o blue)
-# set __fish_prompt_color_user (set_color -o blue)
+set __fish_prompt_color_carat (set_color -o blue)
+set __fish_prompt_color_user (set_color -o blue)
 
 #
 # git prompt config
 #
-# set normal (set_color normal)
-# set magenta (set_color magenta)
-# set yellow (set_color yellow)
-# set green (set_color green)
-# set red (set_color red)
-# set gray (set_color -o black)
+set normal (set_color normal)
+set magenta (set_color magenta)
+set yellow (set_color yellow)
+set green (set_color green)
+set red (set_color red)
+set gray (set_color -o black)
 
 # Fish git prompt
 set __fish_git_prompt_showdirtystate 'yes'
 set __fish_git_prompt_showstashstate 'yes'
 set __fish_git_prompt_showuntrackedfiles 'yes'
 set __fish_git_prompt_showupstream 'yes'
-# set __fish_git_prompt_color_branch yellow
-# set __fish_git_prompt_color_upstream_ahead green
-# set __fish_git_prompt_color_upstream_behind red
+set __fish_git_prompt_color_branch yellow
+set __fish_git_prompt_color_upstream_ahead green
+set __fish_git_prompt_color_upstream_behind red
 
 # Status Chars
 set __fish_git_prompt_char_dirtystate '⚡'
@@ -74,7 +69,7 @@ set __fish_git_prompt_char_upstream_ahead '+'
 set __fish_git_prompt_char_upstream_behind '-'
 
 # Sets the powerline color scheme
-set -g theem_color_scheme dracula
+set -g theme_color_scheme dracula
 # set -g theme_color_scheme terminal-dark
 # set -g theme_color_scheme base16-dark
 # set -g theme_color_scheme solarized
