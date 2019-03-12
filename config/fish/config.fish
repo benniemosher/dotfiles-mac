b7f276969d002f5d045448290b5e64996107b38c
Bennie Mosher <benniemosher@gmail.com>
2019-03-11 20:32:52 -0600

    Update vim config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,33 +1,36 @@
 # Install fundle if not installed
-if not functions -q fundle; eval (curl -sfL https://git.io/fundle-install); end
-
-if test -f ~/.gnupg/.gpg-agent-info -a -n "(pgrep gpg-agent)"; then
-  . ~/.gnupg/gpg-agent-info
-  export GPG_AGENT_INFO
-  set GPG_TTY (tty)
-  export GPG_TTY
-else
-  eval (gpg-agent --daemon | sed -e "s/\(.*\)=\(.*\); export/set -x \1 \2/")
+if not functions -q fundle
+    eval (curl -sfL https://git.io/fundle-install)
 end
 
-bass source ~/.nvm/nvm.sh --no-use ';' nvm use stable
+if test -f ~/.gnupg/.gpg-agent-info -a -n "(pgrep gpg-agent)"
+    then
+    . ~/.gnupg/gpg-agent-info
+    export GPG_AGENT_INFO
+    set GPG_TTY (tty)
+    export GPG_TTY
+else
+    eval (gpg-agent --daemon | sed -e "s/\(.*\)=\(.*\); export/set -x \1 \2/")
+end
 
-# set -g -x PATH ~/Library/Android/sdk/platform-tools $PATH
-# set -g -x PATH ~/Library/Android/sdk/tools $PATH
-# set -g -x ANDROID_HOME ~/Library/Android/sdk
+bass source ~/.nvm/nvm.sh --no-use ';' nvm use --delete-prefix v8.15.0
 
-set -g -x PATH (brew --prefix qt@5.5)/bin $PATH
-set -g -x PATH ~/go $PATH
-
-# set -gx PATH $PATH `yarn global bin`
+source ~/.config/fish/local.config.fish
 
 source /usr/local/share/chruby/chruby.fish
 source /usr/local/share/chruby/auto.fish
-chruby 2.5.0
+chruby 2.6.1
 
 # print the welcome banner
 # fish_logo
 fortune | cowsay -f wolf | lolcat
+
+set -g theme_display_git yes
+set -g theme_display_git_dirty yes
+set -g theme_display_git_untracked yes
+set -g theme_display_git_ahead_verbose yes
+set -g theme_display_git_dirty_verbose yes
+set -g theme_display_git_master_branch yes
 
 #
 # std prompt config
@@ -63,32 +66,34 @@ set __fish_git_prompt_char_upstream_ahead '+'
 set __fish_git_prompt_char_upstream_behind '-'
 
 # Sets the powerline color scheme
+# set -g theme_color_scheme terminal-dark
+set -g theme_color_scheme base16-dark
 # set -g theme_color_scheme solarized
-set -g theme_color_scheme user
-set -l colorfg abb2c0
-set -l dircolor 3E4451
-set __color_initial_segment_exit     ffffff DF6B75 --bold
-set __color_initial_segment_su       ffffff 97C279 --bold
-set __color_initial_segment_jobs     ffffff 61AFEF --bold
+# set -g theme_color_scheme user
+# set -l colorfg abb2c0
+# set -l dircolor 3E4451
+# set __color_initial_segment_exit ffffff DF6B75 --bold
+# set __color_initial_segment_su ffffff 97C279 --bold
+# set __color_initial_segment_jobs ffffff 61AFEF --bold
 
-set __color_path                     $dircolor ffffff
-set __color_path_basename            $dircolor ffffff --bold
-set __color_path_nowrite             d682f0 $colorfg
-set __color_path_nowrite_basename    d682f0 $colorfg --bold
+# set __color_path $dircolor ffffff
+# set __color_path_basename $dircolor ffffff --bold
+# set __color_path_nowrite d682f0 $colorfg
+# set __color_path_nowrite_basename d682f0 $colorfg --bold
 
-set __color_repo                     blue ffffff
-set __color_repo_work_tree           blue ffffff --bold
-set __color_repo_dirty               be5046 ffffff
-set __color_repo_staged              e2c08d ffffff
+# set __color_repo blue ffffff
+# set __color_repo_work_tree blue ffffff --bold
+# set __color_repo_dirty be5046 ffffff
+# set __color_repo_staged e2c08d ffffff
 
-set __color_vi_mode_default          65b6f8 $colorfg --bold
-set __color_vi_mode_insert           abdc89 $colorfg --bold
-set __color_vi_mode_visual           f2ce97 $colorfg --bold
+# set __color_vi_mode_default 65b6f8 $colorfg --bold
+# set __color_vi_mode_insert abdc89 $colorfg --bold
+# set __color_vi_mode_visual f2ce97 $colorfg --bold
 
-set __color_vagrant                  66d7e6 $colorfg
-set __color_username                 ffffff 000000
-set __color_rvm                      61AFEF ffffff --bold
-set __color_virtualfish              65b6f8 $colorfg --bold
+# set __color_vagrant 66d7e6 $colorfg
+# set __color_username ffffff 000000
+# set __color_rvm 61AFEF ffffff --bold
+# set __color_virtualfish 65b6f8 $colorfg --bold
 
 # plugins
 fundle plugin 'oh-my-fish/theme-bobthefish'
@@ -97,7 +102,9 @@ fundle init
 
 # tabtab source for serverless package
 # uninstall by removing these lines or running `tabtab uninstall serverless`
-[ -f /Users/bam/.nvm/versions/node/v10.0.0/lib/node_modules/serverless/node_modules/tabtab/.completions/serverless.fish ]; and . /Users/bam/.nvm/versions/node/v10.0.0/lib/node_modules/serverless/node_modules/tabtab/.completions/serverless.fish
+[ -f /Users/bam/.nvm/versions/node/v10.0.0/lib/node_modules/serverless/node_modules/tabtab/.completions/serverless.fish ]
+and . /Users/bam/.nvm/versions/node/v10.0.0/lib/node_modules/serverless/node_modules/tabtab/.completions/serverless.fish
 # tabtab source for sls package
 # uninstall by removing these lines or running `tabtab uninstall sls`
-[ -f /Users/bam/.nvm/versions/node/v10.0.0/lib/node_modules/serverless/node_modules/tabtab/.completions/sls.fish ]; and . /Users/bam/.nvm/versions/node/v10.0.0/lib/node_modules/serverless/node_modules/tabtab/.completions/sls.fish
+[ -f /Users/bam/.nvm/versions/node/v10.0.0/lib/node_modules/serverless/node_modules/tabtab/.completions/sls.fish ]
+and . /Users/bam/.nvm/versions/node/v10.0.0/lib/node_modules/serverless/node_modules/tabtab/.completions/sls.fish
