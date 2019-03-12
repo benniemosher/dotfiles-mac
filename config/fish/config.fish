87b3587ff9a925d8aa882067c5ddc7403bec4888
Bennie Mosher <benniemosher@gmail.com>
2019-03-12 14:41:43 -0600

    Cleanup extra color things I don't want

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -27,7 +27,7 @@ source /usr/local/share/chruby/auto.fish
 chruby $RUBY_VERSION
 
 # print the welcome banner
-fortune | cowsay -f wolf | lolcat
+fortune | cowsay -f dragon | lolcat
 
 # Set bobthefish theme settings for git
 set -g theme_display_git yes
@@ -37,27 +37,12 @@ set -g theme_display_git_ahead_verbose yes
 set -g theme_display_git_dirty_verbose yes
 set -g theme_display_git_master_branch yes
 
-# std prompt config
-set __fish_prompt_color_carat (set_color -o blue)
-set __fish_prompt_color_user (set_color -o blue)
-
-# git prompt config
-set normal (set_color normal)
-set magenta (set_color magenta)
-set yellow (set_color yellow)
-set green (set_color green)
-set red (set_color red)
-set gray (set_color -o black)
-
 # Fish git prompt
 set __fish_git_prompt_showdirtystate 'yes'
 set __fish_git_prompt_showstashstate 'yes'
 set __fish_git_prompt_showuntrackedfiles 'yes'
 set __fish_git_prompt_showupstream 'yes'
-set __fish_git_prompt_color_branch yellow
-set __fish_git_prompt_color_upstream_ahead green
-set __fish_git_prompt_color_upstream_behind red
-
+#
 # Status Chars
 set __fish_git_prompt_char_dirtystate '⚡'
 set __fish_git_prompt_char_stagedstate '→'
