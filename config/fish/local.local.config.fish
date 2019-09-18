dba58700b729e3fa8fa0d0e9f7c51c394460536c
Bennie Mosher <337539+benniemosher@users.noreply.github.com>
2019-09-18 06:23:57 -0600

    Create local.local.config.fish

---
+++ config/fish/local.local.config.fish
@@ -0,0 +1,8 @@
+set -gx NODE_VERSION --lts
+set -gx RUBY_VERSION --latest
+
+set -gx AWS_DEFAULT_PROFILE mfa
+set -gx AWS_PROFILE mfa
+set -gx AWS_REGION us-east-2
+
+set PATH /Users/bam/Code/nutrien/tfe-cli/bin $PATH .
